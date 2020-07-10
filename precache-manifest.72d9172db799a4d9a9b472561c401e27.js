89f0f94d22f984697a3e05dc197eefad7ab0426f
Sangram Nandkhile <sangram.nandkhile@gmail.com>
2020-07-10 13:17:10 +1000

    Updates

---
+++ precache-manifest.72d9172db799a4d9a9b472561c401e27.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c1eeee860be0883d41db59fd9dc2c9",
+    "url": "/Covid19Australia-ReactJS/index.html"
+  },
+  {
+    "revision": "6ce8a3b195271771560c",
+    "url": "/Covid19Australia-ReactJS/static/css/main.4970dcad.chunk.css"
+  },
+  {
+    "revision": "fc9cb0f0dd7f0f5d3323",
+    "url": "/Covid19Australia-ReactJS/static/js/2.ef0d1bc5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Australia-ReactJS/static/js/2.ef0d1bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce8a3b195271771560c",
+    "url": "/Covid19Australia-ReactJS/static/js/main.cc77d3e7.chunk.js"
+  },
+  {
+    "revision": "445e1cb4e8b38a0fa65e",
+    "url": "/Covid19Australia-ReactJS/static/js/runtime-main.bb4415ba.js"
+  },
+  {
+    "revision": "02155d96e4a3f18305ab944925389c77",
+    "url": "/Covid19Australia-ReactJS/static/media/archia-regular-webfont.02155d96.woff2"
+  },
+  {
+    "revision": "a1d4030b69afb12540b19be94dcfba1e",
+    "url": "/Covid19Australia-ReactJS/static/media/au_map.a1d4030b.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Covid19Australia-ReactJS/static/media/react-logo.5d5d9eef.svg"
+  }
+]);
